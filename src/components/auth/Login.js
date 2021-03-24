3f620e610a6dfb5209dc943ab7a9ea403ce6bf3d
Paul Moreland <paul@pamosystems.com>
2021-03-24 21:03:14 +0100

    Added tag input and icon on login screen

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -6,18 +6,22 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useLoginMutation } from 'models/auth/auth.js';
 import { useMessage } from 'components/messageSystem/Message';
-import { Grid, Typography, FormControlLabel, Checkbox, Button, TextField } from '@material-ui/core';
+import BarcodeReader from 'react-barcode-reader';
+import scanTag from 'assets/img/scan-tag.png';
+import getIcon from 'models/icon/Icon';
+import { Grid, Box, Typography, FormControlLabel, Checkbox, Button, TextField } from '@material-ui/core';
 import useStyles from './Styles';
 
 const Login = () => {
     const title = "Logga in",
+          [tag, setTag] = useState(false),
           [check, setCheck] = useState(false),
           [password, setPassword] = useState(""),
           [login] = useLoginMutation(),
           history = useHistory(),
           messageContext = useMessage(),
           setMessage = messageContext.setMessage,
-          classes = useStyles();
+          classes = useStyles({ tag });
 
     const logon = (e) => {
         e.preventDefault();
@@ -25,7 +29,7 @@ const Login = () => {
         const formData = new FormData(e.target);
 
         let person = {
-            "username": formData.get("username"),
+            "username": tag,
             "password": formData.get("password")
         };
 
@@ -43,67 +47,88 @@ const Login = () => {
             </Grid>
 
             <Grid container spacing={4} justify="center" className="page">
-                <Grid item xs={12} sm={6}>
-                    <form className="form" onSubmit={logon}>
-                        <TextField
-                            id="person-username"
-                            name="username"
-                            label="Tagg"
-                            type="number"
-                            size="small"
-                            variant="filled"
-                            required
-                            InputProps={{ disableUnderline: true }}
-                        />
-
-                        <TextField
-                            className="password"
-                            id="person-password"
-                            name="password"
-                            label="Pinkod"
-                            type={!check ? "password" : "text"}
-                            size="small"
-                            variant="filled"
-                            required
-                            value={password}
-                            onChange={(e) => /^[0-9]{0,4}$/.test(e.target.value) && setPassword(e.target.value)}
-                            InputProps={{ disableUnderline: true }}
-                        />
-
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={check}
-                                    onChange={() => setCheck(!check)}
-                                    name="checkedB"
-                                    style={{ color: "white" }}
-                                />
-                            }
-                            label={`${check ? "Dölja" : "Visa"} pin`}
+                {!tag
+                    ?
+                    <Grid item xs={12} sm={6}>
+                        <BarcodeReader
+                            onError={value => setTag(value)}
                         />
                         
-                        <Button
-                            name="login"
-                            type="submit"
-                            color="primary"
-                            size="large"
-                            variant="contained"
-                        >
-                            Logga in
-                        </Button>
+                        <Typography variant="h3" align="center" gutterBottom>
+                            Skanna din tagg
+                        </Typography>
 
-                        <Button
-                            name="forgot"
-                            type="submit"
-                            color="secondary"
-                            size="large"
-                            variant="contained"
-                            onClick={() => history.push("/forgot")}
-                        >
-                            Glömt pin?
-                        </Button>
-                    </form>
-                </Grid>
+                        <Box className={classes.tag}>
+                            <img src={scanTag} alt="Tag" />
+                        </Box>
+                    </Grid>
+                    :
+                    <Grid item xs={12} sm={6}>
+                        <form className="form" onSubmit={logon}>
+                            <Typography className={classes.backToScanContainer} variant="h4" align="center" gutterBottom>
+                                Skriv din pinkod
+                            </Typography>
+
+                            <TextField
+                                className="password"
+                                id="person-password"
+                                name="password"
+                                label="Pinkod"
+                                type={!check ? "password" : "text"}
+                                size="small"
+                                variant="filled"
+                                required
+                                value={password}
+                                onChange={(e) => /^[0-9]{0,4}$/.test(e.target.value) && setPassword(e.target.value)}
+                                InputProps={{ disableUnderline: true }}
+                                autoFocus={true}
+                            />
+
+                            <Box className={classes.spreadBox}>
+                                <Button
+                                    className="backToScan"
+                                    startIcon={getIcon("LeftArrow")}
+                                    onClick={() => setTag("")}
+                                >
+                                    Till Start
+                                </Button>
+
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={check}
+                                            onChange={() => setCheck(!check)}
+                                            name="checkedB"
+                                            style={{ color: "white" }}
+                                        />
+                                    }
+                                    label={`${check ? "Dölja" : "Visa"} pin`}
+                                />
+                            </Box>
+
+                            <Button
+                                name="login"
+                                type="submit"
+                                color="primary"
+                                size="large"
+                                variant="contained"
+                            >
+                                Logga in
+                            </Button>
+
+                            <Button
+                                name="forgot"
+                                type="submit"
+                                color="secondary"
+                                size="large"
+                                variant="contained"
+                                onClick={() => history.push("/forgot")}
+                            >
+                                Glömt pin?
+                            </Button>
+                        </form>
+                    </Grid>
+                }
             </Grid>
         </Grid>
     );
--- src/components/auth/Styles.js
+++ src/components/auth/Styles.js
@@ -3,6 +3,25 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => {
     return {
         pageBanner: {},
+        tag: {
+            maxWidth: "100%",
+            margin: "auto",
+            textAlign: "center",
+            backgroundColor: theme.palette.background.dark,
+            borderRadius: "10px",
+            [theme.breakpoints.up('sm')]: {
+                maxWidth: 325
+            },
+        },
+        spreadBox: {
+            display: "flex",
+            justifyContent: "space-between",
+            "& .backToScan": {
+                margin: 0,
+                padding: 0,
+                color: theme.palette.iconButton.primary.main
+            }
+        }
     }
 });
 
